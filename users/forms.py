4f0fcd49487a6a3fee22bcfa9abf6924ab78f6a3
Just dave <chidave@gmail.com>
2024-10-22 00:41:46 +0100

    Add Procfile for Render deployment

---
+++ users/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from localflavor.us.forms import USZipCodeField
+from django.contrib.auth.models import User
+from .widget import CustomPictureImageFieldWidget
+
+
+from .models import Location, Profile
+
+class UserForm(forms.ModelForm):
+    username = forms.CharField(disabled=True) #this will make the user not be able to change the username once set
+    class Meta:
+        model = User
+        fields = ('username', 'first_name','last_name','email')
+
+
+class ProfileForm(forms.ModelForm):
+     photo = forms.ImageField(widget=CustomPictureImageFieldWidget)
+     bio = forms.TextInput()
+     
+     class Meta:
+        model = Profile
+        fields = ('photo', 'bio', 'phone')
+
+       
+       
+
+class LocationForm(forms.ModelForm):
+    address_1 = forms.CharField( required=True)
+    zip_code =  USZipCodeField(required=True)
+    class Meta:
+        model = Location
+        fields = {'address_1', 'address_2', 'city', 'state', 'zip_code'}
